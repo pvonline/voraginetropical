0e8c4aba2d0a251559fa166e5b4ceaddc5cb5ebd
karavallenata <radioelpalaciovallenato@gmail.com>
2022-09-30 16:24:32 -0500

    “commit”

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*____________Inicio Reloj Digital____________*/
+
+const $tiempo = document.querySelector('.tiempo'),
+$fecha = document.querySelector('.fecha');
+
+function digitalClock(){
+    let f = new Date(),
+    dia = f.getDate(),
+    mes = f.getMonth() + 1,
+    anio = f.getFullYear(),
+    diaSemana = f.getDay();
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2)
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+    let semana = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+    let showSemana = (semana[diaSemana]);
+    $fecha.innerHTML = `${showSemana}  ${dia}-${mes}-${anio}`
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
+
+/*____________Final Reloj Digital____________*/
